19da79f33cbe8b0725e1e990d8de5366b061af43
Sergey Evseev <sevseev@gmail.com>
2022-10-31 08:15:20 +0300

    formOriginalSize

--- Form1.cs
+++ Form1.cs
@@ -13,12 +13,31 @@ namespace ColoredButtons
     public partial class Form1 : Form
     {
         //original measurements of buttons
-        
-        private Rectangle 
+        private Rectangle button1originalRect;
+        private Rectangle button2originalRect;
+        private Rectangle button3originalRect;
+        private Rectangle button4originalRect;
+        private Rectangle button5originalRect;
+        private Rectangle button6originalRect;
+        private Rectangle button7originalRect;
+        private Rectangle button8originalRect;
+        private Rectangle button9originalRect;
+        private Rectangle button10originalRect;
+        private Rectangle button11originalRect;
+        private Rectangle button12originalRect;
+        private Rectangle button13originalRect;
+        private Rectangle button14originalRect;
+        private Rectangle button15originalRect;
+        private Rectangle button16originalRect;
+        private Rectangle button17originalRect;
+
+        //original dimensions of the form
+        private Point
         
         public Form1() 
         {
             InitializeComponent();
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -27,6 +46,11 @@ namespace ColoredButtons
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
